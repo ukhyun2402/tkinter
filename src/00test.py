0bdfe43d60d952352625fdec092844c9e6d34b4b
ukhyun2402 <hyun4911@gmail.com>
2020-05-12 22:17:47 +0900

    Complete main ui and update img

--- src/00test.py
+++ src/00test.py
@@ -1,33 +1,12 @@
-import tkinter as tk
+num = input("정수 입력: ")
+num2=len(num)
+num = int(num)
 
-class Example(tk.Frame):
-    def __init__(self, parent):
-        tk.Frame.__init__(self, parent)
-        self.text = tk.Text(self, wrap="word")
-        self.vsb = tk.Scrollbar(self, orient="vertical", command=self.text.yview)
-        self.text.configure(yscrollcommand=self.text_yview)
-        self.vsb.pack(side="right", fill="y")
-        self.text.pack(side="left", fill="both", expand=True)
+b = []
 
-        # create an info window in the bottom right corner and
-        # inset a couple of pixels
-        self.info = tk.Label(self.text, width=20, borderwidth=1, relief="solid")
-        self.info.place(relx=1.0, rely=1.0, x=-2, y=-2,anchor="se")
-
-    def text_yview(self, *args):
-        ''' 
-        This gets called whenever the yview changes.  For this example
-        we'll update the label to show the line number of the first
-        visible row. 
-        '''
-        # first, update the scrollbar to reflect the state of the widget
-        self.vsb.set(*args)
-
-        # get index of first visible line, and put that in the label
-        # index = self.text.index("@0,0")
-        # self.info.configure(text=index)
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    Example(root).pack(side="top", fill="both", expand=True)
-    root.mainloop()
+for i in range(2*num2+num2):
+    b.append(num%2)
+    num=num//2
+    if  num <= 0:
+        break
+print(b)
--- src/0101project.py
+++ src/0101project.py
@@ -8,18 +8,46 @@ def ToRGB(rgb):
 main = Tk()
 main.title("")
 main.configure(bg = 'white')
+main.minsize(400,600)
 main.geometry('400x600+200+200')
 main.iconbitmap(default = r"./img/favicon.ico")
 
-userImg = PhotoImage(file = r"./img/userDef.png").subsample(13)
-userImg = PhotoImage(file = r"./img/user.png").subsample(13)
+userImgDef = PhotoImage(file = r"./img/userG.png")
+userImg = PhotoImage(file = r"./img/user.png")
+
+chatImgDef = PhotoImage(file = r"./img/chatG.png")
+chatImg = PhotoImage(file = r"./img/chat.png")
+
+# hover function
+def enterUserBtn(e):
+    userBtn['image'] = userImg
+
+def leaveUserBtn(e):
+    userBtn['image'] = userImgDef
+    
+def enterChatBtn(e):
+    chatBtn['image'] = chatImg
+
+def leaveChatBtn(e):
+    chatBtn['image'] = chatImgDef
 
 #create navigationBar
-NavigationBar = Frame(main, bg = ToRGB((246,246,246)), relief = 'raised',width = userImg.width()+6)
+NavigationBar = Frame(main, bg = ToRGB((249,249,249)), relief = 'raised',width = userImg.width()*2)
 NavigationBar.pack(fill = Y,side=LEFT)
 
 #Create Menu in NavigationBar
-userBtn = Button(NavigationBar, image= userImg, relief = FLAT)
-userBtn.place(x=0, y=0)
+userBtn = Button(NavigationBar, image= userImgDef, relief = FLAT, bg = ToRGB((249,249,249)))
+userBtn.pack(ipady = 15, ipadx = 20)
+# userBtn.place(y=0)
+userBtn.bind("<Enter>", enterUserBtn)
+userBtn.bind("<Leave>", leaveUserBtn)
 
+chatBtn = Button(NavigationBar, image= chatImgDef, relief = FLAT, bg = ToRGB((249,249,249)))
+chatBtn.pack(ipady = 15, ipadx = 20)
+chatBtn.bind("<Enter>", enterChatBtn)
+chatBtn.bind("<Leave>", leaveChatBtn)
+
+mainFrame = Frame(main, bg ="black", relief = 'raised', width = main.winfo_screenmmwidth())
+mainFrame.pack(fill = Y, side=LEFT)
+print(main.winfo_width(),main.winfo_reqwidth(),main.winfo_vrootwidth(),main.winfo_screenmmwidth())
 main.mainloop()
